3cd29b25e2ca3ee357a101923232c2e16aa56efe
Asa Strong <Asa.Strong@wri.org>
2017-09-19 13:46:09 -0400

    simplify run stats for TCloss

--- run_stats.py
+++ run_stats.py
@@ -3,14 +3,9 @@ from utilities import geojson_to_geostore
 
 def main():
 
-    #get layer_dict
-    layer_dict = geojson_to_geostore.create_geostore_dict('Protected Areas')
-
-    #make request to count TCloss and update layer_dict
-    layer_stats = TcLoss().count_tcloss(layer_dict, 'umd-loss-gain')
-
-    print layer_stats
-    # return layer_stats
+    #make request to count TCloss, update gs
+    TcLoss().update_gs('Protected Areas')
+    print "GS updated with TC loss data"
 
 if __name__ == "__main__":
     main()
