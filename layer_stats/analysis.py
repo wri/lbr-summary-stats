e1a45dc4598eed611b4e70f062cc87896e63243c
Asa Strong <Asa.Strong@wri.org>
2017-09-19 13:44:56 -0400

    add method to update google sheet with layer dict data

--- layer_stats/analysis.py
+++ layer_stats/analysis.py
@@ -1,5 +1,7 @@
 import requests
 
+from utilities import google_sheet as gs
+
 class Analysis(object):
 
     def __init__(self):
@@ -7,14 +9,36 @@ class Analysis(object):
         self.api = "http://production-api.globalforestwatch.org/"
 
     def analyze(self, endpoint, period, geostore):
-        '''master method to send get requests to API'''
+        '''master method to send get requests to API
+        :param endpoint: api endpoint
+        :param period: time period of request YYYY-MM-DD,YYYY-MM-DD
+        :param geostore: unique hash assigned to geojson
+        :return: response from the GFW API'''
 
         api_path = "http://production-api.globalforestwatch.org/" + endpoint
 
         payload = {'period': period, 'geostore': geostore}
 
-        r = requests.get(api_path, params= payload)
+        try:
+            r = requests.get(api_path, params= payload)
+            data = r.json()
+            return data
+        except ValueError, e:
+            print "Value Error when decoding json because {}".format(str(e))
 
-        data = r.json()
+    def update_sheet(self, layer_name, layer_stats):
+        '''method to update google sheet with layer_stats
+        :param layer_name: the dataset of the Stats
+        :param layer_stats: a dictionary of the statistics'''
 
-        return data
+        wks = gs._open_spreadsheet(layer_name)
+
+        total_features = len(layer_stats)
+
+        for id, loss_dict in layer_stats.iteritems():
+            for col_name, loss_val in loss_dict.iteritems():
+                try:
+                    gs.set_value('Name', id, col_name, layer_name, loss_val)
+                    print "loss values set for {0} at {1}".format(id, col_name)
+                except ValueError, e:
+                    print "value error for {0} because {1}".format(id, str(e))
