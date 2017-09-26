90bd8ed5d6fc35ee0f2cbdb8fb866e9fb16eb71f
astrong19 <asastrong19@gmail.com>
2017-09-26 13:51:23 -0400

    add calls to update FMCs and CFs

--- run_stats.py
+++ run_stats.py
@@ -10,7 +10,7 @@ def main():
     #parse command line arguments
     parser = argparse.ArgumentParser(description='Separate Liberia stats calculations in command line')
     parser.add_argument('--run', '-r', required=True, choices=['protected_areas',
-                        'forest_management_conracts', 'community_forests'],
+                        'forest_management_contracts', 'community_forests'],
                         help='the update process to kick off')
     args = parser.parse_args()
 
@@ -20,20 +20,66 @@ def main():
         #get tcloss stats for Protected Areas
         umd_stats = UmdLayer().update_gs('Protected Areas')
 
-        #Get Terra I stats for Protected Areas
+        #Get Terra I stats for Protected Areas | CURRENT ERROR POINT
         #Todo: fix terrai api for geostore requests (e.g., f57027ebca24f1a91eca803392bd5e0d)
         #terrai_stats = TerraiLayer().update_gs('Protected Areas')
 
         #Get Biomass Stats
         biomass_stats = BiomassLayer().update_gs('Protected Areas')
 
-        #combine ditionaries
-        all_stats = Analysis().combine_stats(umd_stats, biomass_stats=biomass_stats)
+        # combine ditionaries | CURRENT ERROR POINT
+        # all_stats = Analysis().combine_stats(biomass_stats=biomass_stats)
 
         #update google spreadsheet with layer stats
-        Analysis().update_sheet('Protected Areas', all_stats)
+        Analysis().update_sheet('Protected Areas', umd_stats)
+        print "GS updated with TC loss data for Portected Areas"
 
-        print "GS updated with TC loss data"
+        Analysis().update_sheet('Protected Areas', biomass_stats)
+        print "GS Updated with Biomass data for Protected Areas"
+
+    elif args.run == 'forest_management_contracts':
+
+        #get tcloss stats for Protected Areas
+        umd_stats = UmdLayer().update_gs('Forest Management Contracts')
+
+        #Get Terra I stats for Protected Areas | CURRENT ERROR POINT
+        #Todo: fix terrai api for geostore requests (e.g., f57027ebca24f1a91eca803392bd5e0d)
+        #terrai_stats = TerraiLayer().update_gs('Protected Areas')
+
+        #Get Biomass Stats
+        biomass_stats = BiomassLayer().update_gs('Forest Management Contracts')
+
+        # combine ditionaries | CURRENT ERROR POINT
+        # all_stats = Analysis().combine_stats(biomass_stats=biomass_stats)
+
+        #update google spreadsheet with layer stats
+        Analysis().update_sheet('Forest Management Contracts', umd_stats)
+        print "GS updated with TC loss data for Forest Management Contracts"
+
+        Analysis().update_sheet('Forest Management Contracts', biomass_stats)
+        print "GS Updated with Biomass data for Forest Management Contracts"
+
+    elif args.run == 'community_forests':
+
+        #get tcloss stats for Protected Areas
+        umd_stats = UmdLayer().update_gs('Community Forests')
+
+        #Get Terra I stats for Protected Areas | CURRENT ERROR POINT
+        #Todo: fix terrai api for geostore requests (e.g., f57027ebca24f1a91eca803392bd5e0d)
+        #terrai_stats = TerraiLayer().update_gs('Protected Areas')
+
+        #Get Biomass Stats
+        biomass_stats = BiomassLayer().update_gs('Community Forests')
+
+        # combine ditionaries | CURRENT ERROR POINT
+        # all_stats = Analysis().combine_stats(biomass_stats=biomass_stats)
+
+        #update google spreadsheet with layer stats
+        Analysis().update_sheet('Community Forests', umd_stats)
+        print "GS updated with TC loss data for Community Forests"
+
+        Analysis().update_sheet('Community Forests', biomass_stats)
+        print "GS Updated with Biomass data for Community Forests"
 
     else:
         print "Argument not found"
