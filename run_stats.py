3338f633eb434bac3312afde03f7fc28ac5e3f88
astrong19 <asastrong19@gmail.com>
2017-10-02 08:35:49 -0400

    add features argument to argparse

--- run_stats.py
+++ run_stats.py
@@ -4,6 +4,7 @@ from layer_stats.analysis import Analysis
 from layer_stats.umd_layer import UmdLayer
 from layer_stats.terrai_layer import TerraiLayer
 from layer_stats.biomass_layer import BiomassLayer
+from utilities import arcgis_online
 
 def main():
 
@@ -12,48 +13,35 @@ def main():
     parser.add_argument('--run', '-r', required=True, choices=['protected_areas',
                         'forest_management_contracts', 'community_forests'],
                         help='the update process to kick off')
+    parser.add_argument('--features', '-f', required=False, help='Features in dataset to update')
     args = parser.parse_args()
 
     #make request to count TCloss, update gs
     if args.run == "protected_areas":
 
         #get tcloss stats for Protected Areas
-        umd_stats = UmdLayer().update_gs('Protected Areas')
+        umd_stats = UmdLayer().update_gs('Protected Areas', features=args.features)
 
-        #Get Terra I stats for Protected Areas | CURRENT ERROR POINT
-        #Todo: fix terrai api for geostore requests (e.g., f57027ebca24f1a91eca803392bd5e0d)
-        #terrai_stats = TerraiLayer().update_gs('Protected Areas')
-
-        #Get Biomass Stats
+        # Get Biomass Stats
         biomass_stats = BiomassLayer().update_gs('Protected Areas')
 
-        # combine ditionaries | CURRENT ERROR POINT
-        # all_stats = Analysis().combine_stats(biomass_stats=biomass_stats)
-
-        #update google spreadsheet with layer stats
+        # update google spreadsheet with layer stats
         Analysis().update_sheet('Protected Areas', umd_stats)
         print "GS updated with TC loss data for Portected Areas"
 
         Analysis().update_sheet('Protected Areas', biomass_stats)
         print "GS Updated with Biomass data for Protected Areas"
 
-        #Downlaod sheet_name
+        # Downlaod sheet_name
         Analysis().download_sheet('Protected Areas')
 
     elif args.run == 'forest_management_contracts':
 
-        #get tcloss stats for Protected Areas
-        umd_stats = UmdLayer().update_gs('Forest Management Contracts')
+        # get tcloss stats for Protected Areas
+        umd_stats = UmdLayer().update_gs('Forest Management Contracts', features=args.features)
 
-        #Get Terra I stats for Protected Areas | CURRENT ERROR POINT
-        #Todo: fix terrai api for geostore requests (e.g., f57027ebca24f1a91eca803392bd5e0d)
-        #terrai_stats = TerraiLayer().update_gs('Protected Areas')
-
-        #Get Biomass Stats
+        # Get Biomass Stats
         biomass_stats = BiomassLayer().update_gs('Forest Management Contracts')
-
-        # combine ditionaries | CURRENT ERROR POINT
-        # all_stats = Analysis().combine_stats(biomass_stats=biomass_stats)
 
         #update google spreadsheet with layer stats
         Analysis().update_sheet('Forest Management Contracts', umd_stats)
@@ -68,17 +56,10 @@ def main():
     elif args.run == 'community_forests':
 
         #get tcloss stats for Protected Areas
-        umd_stats = UmdLayer().update_gs('Community Forests')
-
-        #Get Terra I stats for Protected Areas | CURRENT ERROR POINT
-        #Todo: fix terrai api for geostore requests (e.g., f57027ebca24f1a91eca803392bd5e0d)
-        #terrai_stats = TerraiLayer().update_gs('Protected Areas')
+        umd_stats = UmdLayer().update_gs('Community Forests', features=args.features)
 
         #Get Biomass Stats
         biomass_stats = BiomassLayer().update_gs('Community Forests')
-
-        # combine ditionaries | CURRENT ERROR POINT
-        # all_stats = Analysis().combine_stats(biomass_stats=biomass_stats)
 
         #update google spreadsheet with layer stats
         Analysis().update_sheet('Community Forests', umd_stats)
